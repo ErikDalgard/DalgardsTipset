4f4fdb12baf79ed6468b3c5c6fbec717369d8a58
ErikDalgard <erik.dalgard.6689@student.uu.se>
2026-08-16 17:21:21 +0200

    added initial quick css style and updated html a bit.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
 (async () => {
   const user = await requireAuth();
   if (!user) return; // requireAuth har redan skickat vidare till login
-  document.getElementById("status").textContent = `Inloggad som ${user.username}`;
+  document.getElementById("status").textContent = `Hej ${user.username}!`;
 })();
 
 
 document.getElementById("logout-btn").addEventListener("click", logout);
+document.getElementsById("admin-btn").addEventListener("click", () => {
+  window.location.href = "admin.html";
+})
